2b9cf324077137a0f50c918cb78eac1ec6dbfbae
ongyongen <ongyongen2021@gmail.com>
2022-05-29 19:35:40 +0800

    add temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = Number(((temp - 32) * 5/9).toFixed(1))
+  return newTemp
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = Number(((temp * 9/5) + 32).toFixed(1))
+  return newTemp
 };
 
 // Do not edit below this line
