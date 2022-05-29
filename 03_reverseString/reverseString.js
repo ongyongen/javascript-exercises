8e072438d47ee5c2785df8e47fde0148148f98b2
ongyongen <ongyongen2021@gmail.com>
2022-05-29 18:03:28 +0800

    add reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let finalStr = ''
+    for (let i = str.length-1; i >= 0; i--) {
+        finalStr += str[i]
+    }
+    return finalStr
 };
 
 // Do not edit below this line
