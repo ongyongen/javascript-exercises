a2f3c20bed53c7367f58d9f7fbb2a89c440a9a43
ongyongen <ongyongen2021@gmail.com>
2022-05-29 18:48:04 +0800

    add sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else {
+        let smaller = num1 < num2 ? num1 : num2
+        let bigger = num1 < num2 ? num2 : num1
+        let res = smaller
+        let i = smaller
+        while(i < bigger) {
+            i += 1
+            res += i 
+        }
+        return res
+    }
 };
 
 // Do not edit below this line
