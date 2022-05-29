0538d817a83666cd956f60d6221a56a0f5e583d5
ongyongen <ongyongen2021@gmail.com>
2022-05-29 19:27:55 +0800

    refactor leap year logic to be more succint

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,17 +1,14 @@
 const leapYears = function(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true
-            } else {
-                return false
-            }
-        }
-        return true
-    } else {
-        return false
-    }
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
-    
+
+/* Method
+leap year : 
+- divisible by 4 and not divisible by 100
+- divisible by 400 (divisible by 4, divisible by 100, divisible by 400) 
+  (ie if it is divisible by 400, means it is divisible by 4 and 100)
+*/ 
+
 // Do not edit below this line
 module.exports = leapYears;
+
