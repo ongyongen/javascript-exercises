ee797d64c0c0d44531a9a1281f1b8ee59818b98f
ongyongen <ongyongen2021@gmail.com>
2022-05-31 02:22:16 +0800

    add calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let num of arr) {
+    product *= num
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let product = 1;
+  let i = 0;
+  while (i < power) {
+    product *= base;
+    i += 1;
+  }
+	return product
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num == 0) {
+    return 1
+  } else {
+    return num * factorial(num-1) // num * (num - 1) * (num - 2) .. * 1 * 1 
+  }
 	
 };
 
