a6a6b664cab8bb5b8f3c79bab10339ec7d2a7c56
ongyongen <ongyongen2021@gmail.com>
2022-05-31 02:29:39 +0800

    add palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    text = text.toLowerCase()
+    text = text.replace(/ /g,'')
+    let i = 0;
+    let j = text.length - 1;
+    while (i < text.length) {
+        while (j >= 0) {
+            if (text[i] != text[j]) {
+                return false
+            }
+            i += 1
+            j -= 1
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
