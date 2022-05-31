1ce5cebb986fc10be17af2581154017cc3811c4a
ongyongen <ongyongen2021@gmail.com>
2022-05-31 10:43:18 +0800

    add fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,40 @@
-const fibonacci = function() {
+// Recursive solution
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        if (num == 1 || num == 2) {
+            return 1
+        } else {
+            return fibonacci(num-2) + fibonacci(num-1)
+        }
+    }
+}
 
-};
+
+/* Iterative solution 
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        let arr = [];
+        let i = 0
+        while (i < num) {
+            if ((i == 0) || (i == 1)) {
+                arr.push(1)
+            } else {
+                num1 = arr[i-2]
+                num2 = arr[i-1]
+                arr.push(num1 + num2)
+            }
+            i += 1
+        }
+        return arr[num-1]
+    } 
+}
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
