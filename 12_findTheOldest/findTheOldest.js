91a7335a12ba74059ba2eabeed615b19f7c71b24
ongyongen <ongyongen2021@gmail.com>
2022-05-31 12:03:27 +0800

    add findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let ages = people.map(person => {
+        if (person.yearOfDeath != undefined) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return new Date().getFullYear() - person.yearOfBirth;
+        }})
+    let maxAge = ages.reduce((a,b) => {
+        return Math.max(a,b)
+    })
+    let maxAgeIndex = ages.indexOf(maxAge)
+    return people[maxAgeIndex]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
