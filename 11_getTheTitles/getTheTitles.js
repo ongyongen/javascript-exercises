87d8872c52465ff1e81d4e2848b39e565db56710
ongyongen <ongyongen2021@gmail.com>
2022-05-31 11:15:45 +0800

    add getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map((book) => book.title)
 };
+
+/* Iterative method 
+const getTheTitles = function(books) {
+    let arr = []
+    for (let book of books) {
+        arr.push(book.title)
+    }
+    return arr
+};
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
